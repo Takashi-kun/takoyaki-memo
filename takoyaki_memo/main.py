290bab9e1fad73519b2c5948546e41f84b8a0850
Takshi-kun <takashi.0628.honda@gmail.com>
2016-12-31 00:11:49 +0900

    fix bind ip

--- takoyaki_memo/main.py
+++ takoyaki_memo/main.py
@@ -8,4 +8,4 @@ app = apps.create_app(config)
 # This is only used when running locally. When running live, gunicorn runs
 # the application.
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='192.168.50.12', port=8080, debug=True)
